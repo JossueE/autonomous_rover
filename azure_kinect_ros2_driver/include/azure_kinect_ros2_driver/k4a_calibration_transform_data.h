74c0ed457b4da71c3300f821254793802881546d
Jossue Patricio Espinoza Tobar <jossue.espinoza@octopy.com>
2026-04-29 17:20:32 -0600

    tf renewed

--- azure_kinect_ros2_driver/include/azure_kinect_ros2_driver/k4a_calibration_transform_data.h
+++ azure_kinect_ros2_driver/include/azure_kinect_ros2_driver/k4a_calibration_transform_data.h
@@ -53,7 +53,7 @@ public:
     std::string camera_base_frame_ = "camera_base";
     std::string rgb_camera_frame_ = "rgb_camera_link";
     std::string depth_camera_frame_ = "depth_camera_link";
-    std::string imu_frame_ = "imu_link";
+    std::string imu_frame_ = "k4a_imu_link";
 
 private:
     void initialize();
